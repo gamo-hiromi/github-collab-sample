cec34d5f6b5feebfad95e6e78caf783bc0de2d49
gamo <hiromigamo955@gmail.com>
2026-06-28 04:13:38 +0900

    Restore review practice code

--- UserController.php
+++ UserController.php
@@ -1,26 +1,16 @@
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\User;
-use Illuminate\Http\Request;
-
-class UserController extends Controller
+public function index()
 {
-    public function index()
-    {
-        $users = User::all();
-        return view('users.index', ['users' => $users]);
-    }
+$users = User::all();
+return view('users.index', ['users' => $users]);
+}
 
-    public function store(Request $request)
-    {
-        $user = new User;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = $request->password;
-        $user->save();
+public function store(Request $request)
+{
+$user = new User;
+$user->name = $request->name;
+$user->email = $request->email;
+$user->password = $request->password;
+$user->save();
 
-        return redirect('/users');
-    }
+return redirect('/users');
 }
